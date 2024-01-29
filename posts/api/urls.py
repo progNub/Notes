3ff57fa40a8de95adc98fbb1ae03450bfbdceb19
Roman <roma.makhunov@gmail.com>
2024-01-29 18:34:31 +0300

    Добавлена авторизация c JWT

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -10,4 +11,7 @@ urlpatterns = [
     path('tag/', views.TagListCreateApiView.as_view(), name='api-list-tags'),
 
     path('auth/', include('djoser.urls.authtoken')),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
