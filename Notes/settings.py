e3352597ba1e6de9a6e0824011c2d2cd915215d3
Roman <roma.makhunov@gmail.com>
2024-02-04 20:48:42 +0300

    Ubuntu (Linux) + django

--- Notes/settings.py
+++ Notes/settings.py
@@ -25,9 +25,9 @@ AUTH_USER_MODEL = "accounts.User"
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-k0*2g+p7gb#*36#9ap9lnzem&$#6voy%nayu1e4(s75-%wmcjh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_SECRET_KEY', '0') == '1'
+DEBUG = os.environ.get('DJANGO_DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- readme.md
+++ readme.md
@@ -1,6 +1,19 @@
+Переменный окружения 
+
+DJANGO_SECRET_KEY
+DJANGO_DEBUG
+DATABASE_NAME
+DATABASE_USER
+DATABASE_PASSWORD
+DATABASE_HOST
+EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER
+REDIS_CACHE_URL
+
 Запустить файл testFaker, он заполнит базу данных тестовыми данными.
 
 Так же там появится Админ:
 
 username: admin
 password: admin
+
