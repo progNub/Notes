2a6a237ca2317aaed405ca359bce5dea7f13cf80
Roman <roma.makhunov@gmail.com>
2024-02-17 16:38:50 +0300

    Добавил логи

--- accounts/views.py
+++ accounts/views.py
@@ -49,12 +49,9 @@ class RegisterUser(CreateView):
 
 
 def confirm_email(request, uidb64: str, token: str):
-    logger.debug('uid64: ', uidb64)
-    logger.debug('token: ', token)
     user_id = force_str(urlsafe_base64_decode(uidb64))
     user = get_object_or_404(User, pk=user_id)
-    logger.debug('user_id: ', user_id)
-    logger.debug('username', user.username)
+    logger.debug(f'user_id: {user_id}')
     if default_token_generator.check_token(user, token):
         logger.debug('Прошел проверку токена', token)
         user.is_active = True
