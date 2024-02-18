6904ed47a747fb5868e939ab093206f6cf739eb1
Roman <roma.makhunov@gmail.com>
2024-02-18 14:42:16 +0300

    Добавлено удаления пользователя если он не подтвердит свой эмайл

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -6,6 +6,17 @@ from accounts.email import ConfirmEmailUserSender
 User = get_user_model()
 
 
+@shared_task()
+def delete_user(user_id):
+    try:
+        user = User.objects.get(id=user_id, is_active=False)
+    except User.DoesNotExist:
+        return 'NOT OK'
+    else:
+        user.delete()
+        return 'OK'
+
+
 @shared_task(ignore_result=True)
 def send_register_email_tasks(domain, user_id, token='') -> None:
     """Отправка сообщения на почту для ее подтверждения"""
