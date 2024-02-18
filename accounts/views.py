56ef5967e734e806867f7041ce1feb8e2586889d
Roman <roma.makhunov@gmail.com>
2024-02-18 14:34:29 +0300

    Изменил подтверждение эмайла, теперь функция view находится в классе RegisterUser

--- accounts/views.py
+++ accounts/views.py
@@ -45,7 +45,6 @@ class RegisterUser(CreateView):
         # работает проверка токена только если генерить его не в таске Celery а во view
         token = default_token_generator.make_token(self.object)
         send_register_email_tasks.delay(domain, self.object.id, token)
-        delete_user.apply_async(args=[self.object.id], coutdown=10)
         return response
 
     @staticmethod
